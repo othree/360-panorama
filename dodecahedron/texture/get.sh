1737bd9be085b2bc69b1ce717c3b04ac059a1a67
othree_kao <othree_kao@htc.com>
2013-06-20 19:23:46 +0800

    Add 12 face polygon reousrce

---
+++ dodecahedron/texture/get.sh
@@ -0,0 +1,13 @@
+wget -O bottom.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=0&pitch=-90&sensor=false'
+wget -O up72.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=72&pitch=-30&sensor=false'
+wget -O up144.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=144&pitch=-30&sensor=false'
+wget -O up216.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=216&pitch=-30&sensor=false'
+wget -O up288.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=288&pitch=-30&sensor=false'
+wget -O up360.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=0&pitch=-30&sensor=false'
+wget -O top.jpg  'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=0&pitch=90&sensor=false'
+wget -O bottom72.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=108&pitch=30&sensor=false'
+wget -O bottom144.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=180&pitch=30&sensor=false'
+wget -O bottom216.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=252&pitch=30&sensor=false'
+wget -O bottom288.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=324&pitch=30&sensor=false'
+wget -O bottom360.jpg 'http://maps.googleapis.com/maps/api/streetview?size=1600x1600&location=62.462865,7.664983&fov=90&heading=36&pitch=30&sensor=false'
+
